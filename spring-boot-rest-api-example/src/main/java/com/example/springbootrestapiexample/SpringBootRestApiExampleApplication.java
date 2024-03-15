df3877108f80d312d5f31799accde8882a9fb292
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2024-03-15 16:36:57 +0000

    Added template spring boot application

---
+++ spring-boot-rest-api-example/src/main/java/com/example/springbootrestapiexample/SpringBootRestApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrestapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRestApiExampleApplication.class, args);
+  }
+
+}
---
+++ spring-boot-rest-api-example/src/test/java/com/example/springbootrestapiexample/SpringBootRestApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootrestapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
